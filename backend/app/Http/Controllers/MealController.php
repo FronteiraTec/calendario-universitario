610c1c82072d225c932af3b17248b0066b7a0beb
Mateus Koppe <mateus.koppe@gmail.com>
2019-04-05 14:31:59 -0300

    Create meal controller and model

---
+++ backend/app/Http/Controllers/MealController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Meal;
+use Illuminate\Http\Request;
+
+class MealController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Meal $meal)
+    {
+        //
+    }
+}
