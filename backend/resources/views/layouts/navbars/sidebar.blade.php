6fdae4c6dce199b3294830acf9ce9e5e014cb456
Mateus Koppe <mateus.koppe@gmail.com>
2019-04-10 13:48:22 -0300

    Add meal in sidebar

--- backend/resources/views/layouts/navbars/sidebar.blade.php
+++ backend/resources/views/layouts/navbars/sidebar.blade.php
@@ -83,26 +83,11 @@
                         <i class="ni ni-tv-2 text-primary"></i> {{ __('Dashboard') }}
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-examples">
-                        <i class="fab fa-laravel" style="color: #f4645f;"></i>
-                        <span class="nav-link-text" style="color: #f4645f;">{{ __('Laravel Examples') }}</span>
-                    </a>
 
-                    <div class="collapse show" id="navbar-examples">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('profile.edit') }}">
-                                    {{ __('User profile') }}
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('user.index') }}">
-                                    {{ __('User Management') }}
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('meal.index') }}">
+                        <i class="ni ni-single-copy-04 text-primary"></i> Cardápios
+                    </a>
                 </li>
             </ul>
         </div>
