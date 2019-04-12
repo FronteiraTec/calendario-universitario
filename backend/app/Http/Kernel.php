e672180327a0b18f427dec01749e3576ea55d171
Mateus Koppe <mateus.koppe@gmail.com>
2019-04-12 19:22:58 -0300

    CORS in backend

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            \App\Http\Middleware\Cors::class,
         ],
     ];
 
@@ -60,6 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 
     /**
