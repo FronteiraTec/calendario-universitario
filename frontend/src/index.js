86141ba0938e0168437c0bc48d4a716ed6382a64
Mateus Koppe <mateus.koppe@gmail.com>
2019-04-13 05:28:44 -0300

    Add fontawesome

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css'
+import '@fortawesome/fontawesome-free/css/solid.css'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
