75e0e4ff7b4fcb5402a679a0345336488e04c9c9
Mateus Koppe <mateus.koppe@gmail.com>
2019-04-13 05:28:57 -0300

    Create base Header

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import AppHader from './AppHeader'
 
 import MealList from '../containers/MealList'
 
@@ -7,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppHader/>
         <MealList/>
       </div>
     )
---
+++ frontend/src/components/AppHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import './AppHeader.css'
+
+class AppHeader extends Component {
+  render () {
+    return (
+      <header className="AppHeader">
+        <i className="fa fa-bars"></i>
+        Abril
+      </header>
+    )
+  }
+}
+
+export default AppHeader
