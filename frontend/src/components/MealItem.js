7d83169a98dc2e58df293f074904fb91c42f30ae
Mateus Koppe <mateus.koppe@gmail.com>
2019-04-13 00:39:34 -0300

    Show right date

--- frontend/src/components/MealItem.js
+++ frontend/src/components/MealItem.js
@@ -1,15 +1,40 @@
 import React from 'react'
 import "./MealItem.css"
 
-export default (props) => {
+const getWeekDay = dateProp => {
+  const date = new Date(dateProp)
+  switch (date.getDay()) {
+    case 0:
+      return 'Dom'
+    case 1:
+      return 'Seg'
+    case 2:
+      return 'Ter'
+    case 3:
+      return 'Qua'
+    case 4:
+      return 'Qui'
+    case 5:
+      return 'Sex'
+    case 6:
+      return 'Sab'
+  }
+}
+
+const getDateNumber = dateProp => {
+  const date = new Date(dateProp)
+  return date.getDay()
+}
+
+const MealItem = (props) => {
   return (
     <div className="MealItem">
       <div className="MealItem__day">
         <div className="MealItem__week-day">
-          Qua
+          { getWeekDay(props.day) }
         </div>
         <div className="MealItem__day-number">
-          { (props.day || '- - - ').split("-")[2].split(' ')[0] }
+          { getDateNumber(props.day) }
         </div>
       </div>
       <div className="MealItem__description">
@@ -18,3 +43,5 @@ export default (props) => {
     </div>
   )
 }
+
+export default MealItem
