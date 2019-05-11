4738992efde5b76e5472a3c0a51e2b3aa4353335
Mateus Koppe <mateus.koppe@gmail.com>
2019-05-11 20:51:56 -0300

    Swipe menu to close

--- frontend/src/components/AppMenu.js
+++ frontend/src/components/AppMenu.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import Swipe from 'react-easy-swipe'
 
 import './AppMenu.css'
+
+const handleSwipeMove = handler => event => {
+  if (event.x < -70) {
+    handler()
+  }
+}
 
 const AppMenu = (props) => (
   <div className={`AppMenu ${props.isOpened ? 'AppMenu--opened' : ''}`}>
@@ -8,19 +15,23 @@ const AppMenu = (props) => (
       className="AppMenu__cover"
       onClick={() => props.handleCloseMenu()}
     ></div>
-    <div className="AppMenu__content">
-      <h3 className="AppMenu__title">Calendário UFFS</h3>
-      <nav className="AppMenu__list">
-        <span className="AppMenu__item AppMenu__item--active">
-          <i className="AppMenu__icon fa fa-calendar-alt"></i>
-          Calendário
-        </span>
-        <span className="AppMenu__item">
-          <i className="AppMenu__icon fa fa-info"></i>
-          Sobre
-        </span>
-      </nav>
-    </div>
+    <Swipe
+      onSwipeMove={handleSwipeMove(props.handleCloseMenu)}
+    >
+      <div className="AppMenu__content">
+        <h3 className="AppMenu__title">Calendário UFFS</h3>
+        <nav className="AppMenu__list">
+          <span className="AppMenu__item AppMenu__item--active">
+            <i className="AppMenu__icon fa fa-calendar-alt"></i>
+            Calendário
+          </span>
+          <span className="AppMenu__item">
+            <i className="AppMenu__icon fa fa-info"></i>
+            Sobre
+          </span>
+        </nav>
+      </div>
+    </Swipe>
   </div>
 )
 
