3cf4d79ce17fdda18aab44f73081b5981f8f037f
Mateus Koppe <mateus.koppe@gmail.com>
2019-05-13 16:02:38 -0300

    About base page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import './About.css'
+
 const About = () => (
-  <div>
+  <div className="About">
     <h2>Sobre</h2>
+    <img width="130px" src="https://avatars3.githubusercontent.com/u/4642782?s=400&v=4"/>
+    <h3>Open Source</h3>
+    <i style={{fontSize: 50}} className="fab fa-github"></i>
   </div>
 )
 
