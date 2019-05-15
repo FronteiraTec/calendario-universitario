cc6a16dd0191b6fc251e6cf43a608107e0cef949
Mateus Koppe <mateus.koppe@gmail.com>
2019-05-15 18:01:41 -0300

    Create handlePermission middleware

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -54,6 +54,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'handlePermissions' => \App\Http\Middleware\HandlePermissions::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
