090fab3773666005f7def4b8317c2db6e808b6b4
Junior-16 <junior.ramisch@gmail.com>
2019-05-23 00:34:00 -0300

    adding FronteiraTec and Calendario UFFS info

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -5,8 +5,24 @@ import './About.css'
 const About = () => (
   <div className="About">
     <h2>Sobre</h2>
-    <img width="130px" src="https://avatars3.githubusercontent.com/u/4642782?s=400&v=4"/>
-    <h3>Open Source</h3>
+    <div className = "FronteiraInfo">
+        <h3> FronteiraTec </h3>
+        <p>
+            FronteiraTec é uma empresa júnior do curso de Ciência da Computação
+            da Universidade Federal Da Fronteira Sul. Ela é constituída
+            essencialmente por professores e estudantes do curso. Seu objetivo principal
+            é encorajar as idéias empreendedoras que surgem entre os alunos e torná-las realidade.
+        </p>
+
+        <h3>Calendário UFFS</h3>
+        <p>
+            O projeto Calendário UFFS objetiva tornar prático o acesso ao
+            Cardápio do RU e aos eventos que ocasionalmente ocorrem no meio
+            acadêmico da UFFS Campus Chapecó. Ele foi desenvolvido pela célula Web
+            da FronteiraTec. O projeto atualmente é de código aberto... 
+        </p>
+    </div>
+    <img width="50px" src="https://avatars3.githubusercontent.com/u/4642782?s=400&v=4"/>
     <i style={{fontSize: 50}} className="fab fa-github"></i>
   </div>
 )
