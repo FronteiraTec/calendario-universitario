3d1b024739e3758b68fa8f0252cb18bd9454882c
Junior-16 <junior.ramisch@gmail.com>
2019-05-26 17:11:38 -0300

    code review requiremets

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -3,9 +3,13 @@ import './About.css'
 
 const About = () => (
   <div className="About">
-    <h2>Sobre</h2>
-    <div className = "FronteiraInfo">
+
+    <h2> Sobre </h2>
+
+    <div className = "About__info">
+
         <h3> FronteiraTec </h3>
+
         <p>
             FronteiraTec é uma empresa júnior do curso de Ciência da Computação
             da Universidade Federal Da Fronteira Sul. Ela é constituída
@@ -16,28 +20,38 @@ const About = () => (
             implementação de soluções baseadas em software.
         </p>
 
-        <h3>Calendário UFFS</h3>
+        <h3> Calendário UFFS </h3>
+
         <p>
             O projeto Calendário UFFS objetiva tornar prático o acesso ao
             Cardápio do RU e aos eventos que ocasionalmente ocorrem no meio
             acadêmico da UFFS Campus Chapecó. Calendário UFFS é uma aplicação de
             código aberto que atualmente encontra-se hospedado na
-            plataforma <a href="https://github.com/FronteiraTec/calendario-uffs"
-            style={{fontSize: 15, color: "#0366d6"}}>GitHub</a>. A intenção de tornar
-            a aplicação <i>Open Source</i> é de grande importância pois dessa maneira
-            qualquer desenvolvedor é capaz de contribuir com alguma idéia para o melhoramento
-            do projeto, o que reflete diretamente a comunidade acadêmica que dispõe
-            desse recurso.
+            plataforma <a className = "About__text-link" target="_blank"
+            href=" https://github.com/FronteiraTec/calendario-uffs">GitHub</a>.
+            A intenção de tornar a aplicação <i>Open Source</i> é de grande
+            importância pois dessa maneira qualquer desenvolvedor é capaz de
+            contribuir com alguma ideia para o melhoramento do projeto, o que
+            reflete diretamente a comunidade acadêmica que dispõe desse recurso.
         </p>
+
+        <a  className = "About__text-link" target="_blank" href="http://fronteiratec.com/">
+            FronteiraTec Inc.
+        </a>
+
+        <div className = "About__icon-link">
+
+            <a target="_blank" href = "https://github.com/FronteiraTec/calendario-uffs">
+                <i className = "fab fa-github About__icon-link--github"></i>
+            </a>
+
+            <a target="_blank" href = "http://fronteiratec.com/">
+                <img className = "About__icon-link--fronteira"  src={process.env.PUBLIC_URL + '/logo_completo_fronteira.png'}/>
+            </a>
+            
+        </div>
     </div>
-		<a href="http://fronteiratec.com/" style={{fontSize: 15, color: "#0366d6"}}>FronteiraTec Inc.</a>
-		<a href = "https://github.com/FronteiraTec/calendario-uffs" style={{float:"right"}}>
-			<i style={{fontSize: 30, color: "black", marginLeft: ".7rem"}} className="fab fa-github"></i>
-		</a>
-		<a href = "http://fronteiratec.com/" style={{float:"right"}}>
-			<img style={{width: 120, height: 30}} src={process.env.PUBLIC_URL + '/logo_completo_fronteira.png'}/>
-		</a>
-  </div>
+</div>
 )
 
 export default About
